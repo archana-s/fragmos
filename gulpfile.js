98ec4e49d3c4beb47ab8c1a358439dd91d862530
ArchanaS <archana.june@gmail.com>
2016-11-13 17:09:08 -0800

    polishing the UI

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,28 @@ gulp.task('build:styleguide', function () {
     var concat = require('gulp-concat')
     var postcss = require('gulp-postcss')
     var autoprefixer = require('autoprefixer')
-    var customProperties = require('postcss-custom-properties')
+    var customProperties = require('postcss-css-variables')
     var Import = require('postcss-import')
     var styleGuide = require('postcss-style-guide')
     var extend = require('postcss-extend')
     var nano = require('cssnano')
 
     return gulp.src('./styles/app.css')
-        .pipe(postcss([
-            Import,
-            extend,
-            customProperties({ preserve: true }),
-            autoprefixer,
-            styleGuide({
-                project: 'Styleguide',
-                dest: 'styleguide/index.html',
-                showCode: false,
-                themePath: './node_modules/psg-theme-default/'
-            }),
-            nano
-        ]))
-        .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('dist/styles'))
+      .pipe(postcss([
+          Import,
+          extend,
+          customProperties({ preserve: true }),
+          autoprefixer,
+          styleGuide({
+              project: 'Styleguide',
+              dest: 'styleguide/index.html',
+              showCode: false,
+              themePath: './styleguide-template/'
+          }),
+          nano
+      ]))
+      .pipe(concat('app.min.css'))
+      .pipe(gulp.dest('dist/styles'))
 })
 
 gulp.task('default', function() {
