3a3150f9398f4514c8d1ec00377b3cd15713cd8b
ArchanaS <archana.june@gmail.com>
2017-03-26 16:11:41 -0700

    Updated setup

--- setup.js
+++ setup.js
@@ -5,10 +5,10 @@ var rl = require('readline');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 // Create a directory one level above fragmos
-var styleguideExists = fs.existsSync('../proj_styleguide');
+var styleguideExists = fs.existsSync('../styleguide');
 
 if (styleguideExists) {
-  console.log('proj_styleguide folder exists. Please rename it and try again.');
+  console.log('styleguide folder exists. Please rename it and try again.');
 }
 
 if (!styleguideExists) {
@@ -20,33 +20,33 @@ if (!styleguideExists) {
       i.question("Provide a local full path to your logo file (You can also leave it at default for now): ", function (logoLocation) {
 
         // Create the project Styleguide directory
-        fs.mkdir('../proj_styleguide', function (data) {
-          copy('./src', '../proj_styleguide/src', function (err, results) {
+        fs.mkdir('../styleguide', function (data) {
+          copy('./src', '../styleguide/src', function (err, results) {
             if (err) {
               throw err;
             }
 
             // Add package.json and index.js files
-            fs.createReadStream('package.json.toCopy').pipe(fs.createWriteStream('../proj_styleguide/package.json'));
-            fs.createReadStream('index.js').pipe(fs.createWriteStream('../proj_styleguide/index.js'));
+            fs.createReadStream('package.json.toCopy').pipe(fs.createWriteStream('../styleguide/package.json'));
+            fs.createReadStream('index.js').pipe(fs.createWriteStream('../styleguide/index.js'));
 
             // Include the project name in gulpfile
             var gulpFileData = fs.readFileSync('gulpfile.js', 'utf-8');
             gulpFileData = gulpFileData.replace('%%project%%', projectName);
             try {
-              fs.writeFileSync('../proj_styleguide/gulpfile.js', gulpFileData);
+              fs.writeFileSync('../styleguide/gulpfile.js', gulpFileData);
             } catch (err) {
               throw err;
             }
 
-            copy('./public', '../proj_styleguide/public', function (err, data) {
+            copy('./public', '../styleguide/public', function (err, data) {
               if (err) throw err;
               if (logoLocation && fs.existsSync(logoLocation)) {
-                fs.createReadStream(logoLocation).pipe(fs.createWriteStream('../proj_styleguide/public/images/logo.png'));
+                fs.createReadStream(logoLocation).pipe(fs.createWriteStream('../styleguide/public/images/logo.png'));
               } else {
                 console.log('Could not find the logo file at ', logoLocation, '. Default icon will be included now.');
               }
-              console.log('You can now delete this directory. You generated styleguide + all styles are available in proj_styleguide dir');
+              console.log('You can now delete this directory. You generated styleguide + all styles are available in styleguide dir');
               i.close();
             });
           });
