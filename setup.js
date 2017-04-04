2c122e306550ae4fd08459d3495da1fffbb7c0c6
ArchanaS <archana.june@gmail.com>
2017-04-03 21:48:03 -0700

    Refined setup and runs with command fragmos once installed

--- setup.js
+++ setup.js
@@ -1,57 +1,72 @@
+#!/usr/bin/env node
 'use strict';
 
-var copy = require('recursive-copy');
-var rl = require('readline');
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-// Create a directory one level above fragmos
-var styleguideExists = fs.existsSync('../styleguide');
+var npm = require('npm');
 
-if (styleguideExists) {
-  console.log('styleguide folder exists. Please rename it and try again.');
-}
+npm.load(function (err) {
+  // Install necessary modules to set up styleguide
+  npm.commands.install(['recursive-copy', 'readline', 'fs'], function (err, data) {
+    if (err) {
+      throw err;
+    }
 
-if (!styleguideExists) {
-  (function () {
-    var i = rl.createInterface(process.stdin, process.stdout, null);
+    var copy = require('recursive-copy');
+    var rl = require('readline');
+    var fs = require('fs');
 
-    // Ask the user for project name and logo location
-    i.question("What is the name of your project? ", function (projectName) {
-      i.question("Provide a local full path to your logo file (You can also leave it at default for now): ", function (logoLocation) {
+    // Create a directory one level above fragmos
+    var styleguideExists = fs.existsSync('./proj_styleguide');
 
-        // Create the project Styleguide directory
-        fs.mkdir('../styleguide', function (data) {
-          copy('./src', '../styleguide/src', function (err, results) {
-            if (err) {
-              throw err;
-            }
+    if (styleguideExists) {
+      console.log('proj_styleguide folder exists. Please rename it and try again.');
+    }
 
-            // Add package.json and index.js files
-            fs.createReadStream('package.json.toCopy').pipe(fs.createWriteStream('../styleguide/package.json'));
-            fs.createReadStream('index.js').pipe(fs.createWriteStream('../styleguide/index.js'));
+    if (!styleguideExists) {
+      (function () {
+        var i = rl.createInterface(process.stdin, process.stdout, null);
 
-            // Include the project name in gulpfile
-            var gulpFileData = fs.readFileSync('gulpfile.js', 'utf-8');
-            gulpFileData = gulpFileData.replace('%%project%%', projectName);
-            try {
-              fs.writeFileSync('../styleguide/gulpfile.js', gulpFileData);
-            } catch (err) {
-              throw err;
-            }
+        // Ask the user for project name and logo location
+        i.question("What is the name of your project? ", function (projectName) {
+          i.question("Provide a local full path to your logo file (You can also leave it at default for now): ", function (logoLocation) {
 
-            copy('./public', '../styleguide/public', function (err, data) {
-              if (err) throw err;
-              if (logoLocation && fs.existsSync(logoLocation)) {
-                fs.createReadStream(logoLocation).pipe(fs.createWriteStream('../styleguide/public/images/logo.png'));
-              } else {
-                console.log('Could not find the logo file at ', logoLocation, '. Default icon will be included now.');
-              }
-              console.log('You can now delete this directory. You generated styleguide + all styles are available in styleguide dir');
-              i.close();
+            // Create the project Styleguide directory
+            fs.mkdir('./proj_styleguide', function (data) {
+              copy('/usr/local/lib/node_modules/fragmos/src', './proj_styleguide/src', function (err, results) {
+                if (err) {
+                  throw err;
+                }
+
+                // Add package.json and index.js files
+                fs.createReadStream('/usr/local/lib/node_modules/fragmos/package.json.toCopy').pipe(fs.createWriteStream('./proj_styleguide/package.json'));
+                fs.createReadStream('/usr/local/lib/node_modules/fragmos/index.js').pipe(fs.createWriteStream('./proj_styleguide/index.js'));
+
+                // Include the project name in gulpfile
+                var gulpFileData = fs.readFileSync('/usr/local/lib/node_modules/fragmos/gulpfile.js', 'utf-8');
+                gulpFileData = gulpFileData.replace('%%project%%', projectName);
+                try {
+                  fs.writeFileSync('./proj_styleguide/gulpfile.js', gulpFileData);
+                } catch (err) {
+                  throw err;
+                }
+
+                copy('/usr/local/lib/node_modules/fragmos/public', './proj_styleguide/public', function (err, data) {
+                  if (err) throw err;
+                  if (logoLocation && fs.existsSync(logoLocation)) {
+                    fs.createReadStream(logoLocation).pipe(fs.createWriteStream('./proj_styleguide/public/images/logo.png'));
+                  } else {
+                    console.log('Could not find the logo file at ', logoLocation, '. Default icon will be included now.');
+                  }
+                  console.log('You generated styleguide + all styles are available in proj_styleguide dir');
+                  i.close();
+                });
+              });
             });
           });
         });
-      });
-    });
-  })();
-}
+      })();
+    }
+  });
+  npm.on('log', function (message) {
+    console.log(message);
+  });
+});
--- setup_dev.js
+++ setup_dev.js
@@ -1,53 +1,69 @@
-const copy = require('recursive-copy')
-const rl = require('readline')
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-// Create a directory one level above fragmos
-const styleguideExists = fs.existsSync('../proj_styleguide');
+#!/usr/bin/env node
 
-if (styleguideExists) {
-  console.log('proj_styleguide folder exists. Please rename it and try again.')
-}
+const npm = require('npm')
 
-if (!styleguideExists) {
-  const i = rl.createInterface(process.stdin, process.stdout, null)
+npm.load(err => {
+  // Install necessary modules to set up styleguide
+  npm.commands.install(['recursive-copy', 'readline', 'fs'], (err, data) => {
+    if (err) {
+      throw err;
+    }
 
-  // Ask the user for project name and logo location
-  i.question("What is the name of your project? ", projectName => {
-    i.question("Provide a local full path to your logo file (You can also leave it at default for now): ", logoLocation => {
+    const copy = require('recursive-copy')
+    const rl = require('readline')
+    const fs = require('fs')
 
-      // Create the project Styleguide directory
-      fs.mkdir('../proj_styleguide', function(data) {
-        copy('./src', '../proj_styleguide/src', function(err, results) {
-          if (err) {
-            throw err;
-          }
+    // Create a directory one level above fragmos
+    const styleguideExists = fs.existsSync('./proj_styleguide');
 
-          // Add package.json and index.js files
-          fs.createReadStream('package.json.toCopy').pipe(fs.createWriteStream('../proj_styleguide/package.json'));
-          fs.createReadStream('index.js').pipe(fs.createWriteStream('../proj_styleguide/index.js'));
+    if (styleguideExists) {
+      console.log('proj_styleguide folder exists. Please rename it and try again.')
+    }
 
-          // Include the project name in gulpfile
-          let gulpFileData = fs.readFileSync('gulpfile.js', 'utf-8')
-          gulpFileData = gulpFileData.replace('%%project%%', projectName)
-          try {
-            fs.writeFileSync('../proj_styleguide/gulpfile.js', gulpFileData)
-          } catch(err) {
-            throw err;
-          }
+    if (!styleguideExists) {
+      const i = rl.createInterface(process.stdin, process.stdout, null)
 
-          copy('./public', '../proj_styleguide/public', function(err, data) {
-            if (err) throw err;
-            if (logoLocation && fs.existsSync(logoLocation)) {
-              fs.createReadStream(logoLocation).pipe(fs.createWriteStream('../proj_styleguide/public/images/logo.png'))
-            } else {
-              console.log('Could not find the logo file at ', logoLocation,  '. Default icon will be included now.')
-            }
-            console.log('You can now delete this directory. You generated styleguide + all styles are available in proj_styleguide dir')
-            i.close()
+      // Ask the user for project name and logo location
+      i.question("What is the name of your project? ", projectName => {
+        i.question("Provide a local full path to your logo file (You can also leave it at default for now): ", logoLocation => {
+
+          // Create the project Styleguide directory
+          fs.mkdir('./proj_styleguide', function(data) {
+            copy('/usr/local/lib/node_modules/fragmos/src', './proj_styleguide/src', function(err, results) {
+              if (err) {
+                throw err;
+              }
+
+              // Add package.json and index.js files
+              fs.createReadStream('/usr/local/lib/node_modules/fragmos/package.json.toCopy').pipe(fs.createWriteStream('./proj_styleguide/package.json'));
+              fs.createReadStream('/usr/local/lib/node_modules/fragmos/index.js').pipe(fs.createWriteStream('./proj_styleguide/index.js'));
+
+              // Include the project name in gulpfile
+              let gulpFileData = fs.readFileSync('/usr/local/lib/node_modules/fragmos/gulpfile.js', 'utf-8')
+              gulpFileData = gulpFileData.replace('%%project%%', projectName)
+              try {
+                fs.writeFileSync('./proj_styleguide/gulpfile.js', gulpFileData)
+              } catch(err) {
+                throw err;
+              }
+
+              copy('/usr/local/lib/node_modules/fragmos/public', './proj_styleguide/public', function(err, data) {
+                if (err) throw err;
+                if (logoLocation && fs.existsSync(logoLocation)) {
+                  fs.createReadStream(logoLocation).pipe(fs.createWriteStream('./proj_styleguide/public/images/logo.png'))
+                } else {
+                  console.log('Could not find the logo file at ', logoLocation,  '. Default icon will be included now.')
+                }
+                console.log('You generated styleguide + all styles are available in proj_styleguide dir')
+                i.close()
+              })
+            })
           })
         })
       })
-    })
+    }
   })
-}
+  npm.on('log', message => {
+    console.log(message)
+  })
+})
